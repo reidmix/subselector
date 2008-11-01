b9f9d794fbc92968f0d3172e6669b26b2e6b88df
Reid MacDonald <reid@laruby.com>
2008-10-31 22:46:00 -0700

    to_proc on raise symbol instead of a proc constant

--- lib/subselector.rb
+++ lib/subselector.rb
@@ -12,9 +12,8 @@ ActiveRecord::Base.class_eval do
         (value.respond_to?(:keys) and (value.keys & SUBSELECT_SQL.keys).first) || nil
       end
       
-      RAISE_ON_NIL = proc { raise 'nil' }
       def extract_subselect_model!(subselect)        
-        subselect.delete(:model, &RAISE_ON_NIL).to_s.classify.constantize rescue self
+        subselect.delete(:model, &:raise).to_s.classify.constantize rescue self
       end
 
       def attribute_condition_with_subselect(argument)
